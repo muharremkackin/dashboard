a5f908618e615183d0f2768a950af8eb01f63d43
muharremkackin01 <muharremkackin01@gmail.com>
2023-01-05 16:00:30 +0300

    responsive of register two components added

--- tailwind.collector.config.js
+++ tailwind.collector.config.js
@@ -12,7 +12,7 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
-      '3xl': '1800px'
+      '3xl': '1536px'
     },
     colors: ({ colors }) => ({
       inherit: colors.inherit,
